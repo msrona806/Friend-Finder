b4765ae2c668fe3fea8fee562b6d968261900d4a
Sharona Young <youngs_e@hotmail.com>
2018-01-18 01:01:46 -0500

    server connection working

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-// Required packages
+// ___________DEPENDENCIES___________
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
+// setup for express app
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+//________________DATA__________________
+
+//______________ ROUTES_________________
+
+
+//______________LISTENER________________
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
